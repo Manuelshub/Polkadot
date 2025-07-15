6125da777960e5379579472976cfbe2b05c5fed3
Emmanuel <kcemmy03@gmail.com>
2025-07-15 11:49:10 +0100

    Modifications to system pallet/module

--- state-machine/src/system.rs
+++ state-machine/src/system.rs
@@ -1,29 +1,37 @@
-use std::collections::BTreeMap;
+use num::{One, Zero};
+use std::{collections::BTreeMap, ops::AddAssign};
 
-pub struct Pallet {
-    block_number: u32,
-    nonce: BTreeMap<String, u32>,
+pub trait Config {
+    type AccuontId: Ord + Clone;
+    type BlockNumber: Zero + One + AddAssign + Copy;
+    type Nonce: Zero + One + Copy;
 }
 
-impl Pallet {
+#[derive(Debug)]
+pub struct Pallet<T: Config> {
+    block_number: T::BlockNumber,
+    nonce: BTreeMap<T::AccuontId, T::Nonce>,
+}
+
+impl<T: Config> Pallet<T> {
     pub fn new() -> Self {
         Self {
-            block_number: 0,
+            block_number: T::BlockNumber::zero(),
             nonce: BTreeMap::new(),
         }
     }
 
-    pub fn block_number(&self) -> u32 {
+    pub fn block_number(&self) -> T::BlockNumber {
         self.block_number
     }
 
     pub fn inc_block_number(&mut self) {
-        self.block_number += 1;
+        self.block_number += T::BlockNumber::one();
     }
 
-    pub fn inc_nonce(&mut self, who: &String) {
-        let current_nonce = self.nonce.get(who).unwrap_or(&0);
-        let new_nonce = current_nonce + 1;
+    pub fn inc_nonce(&mut self, who: &T::AccuontId) {
+        let current_nonce = *self.nonce.get(who).unwrap_or(&T::Nonce::zero());
+        let new_nonce = current_nonce + T::Nonce::one();
         self.nonce.insert(who.clone(), new_nonce);
     }
 }
@@ -31,15 +39,23 @@ impl Pallet {
 #[cfg(test)]
 mod tests {
     use super::Pallet;
+    struct TestConfig;
+    impl super::Config for TestConfig {
+        type AccuontId = String;
+        type BlockNumber = u32;
+        type Nonce = u32;
+    }
 
     #[test]
     fn test_system_pallet() {
-        let mut sys_pallet = Pallet::new();
+        let mut sys_pallet = Pallet::<TestConfig>::new();
 
         // Increase the block number
         sys_pallet.inc_block_number();
         // Increase the nonce
         sys_pallet.inc_nonce(&"Nolan".to_string());
         assert_eq!(sys_pallet.block_number(), 1);
+        assert_eq!(sys_pallet.nonce.get("Nolan"), Some(&1));
+        assert_eq!(sys_pallet.nonce.get("Grey"), None);
     }
 }
