f299e01e2c6d8e0e7a34916798139e01f749e1bc
Emmanuel <kcemmy03@gmail.com>
2025-07-15 11:44:54 +0100

    Revert "Modifications to balances pallet/module"
    This reverts commit 66cdd4199f5cd62766f2af8ce76c482086e9fc1f.

--- state-machine/src/balances.rs
+++ state-machine/src/balances.rs
@@ -1,17 +1,10 @@
-use num::traits::{CheckedAdd, CheckedSub, Zero};
 use std::collections::BTreeMap;
 
-pub trait Config {
-    type AccountId: Ord + Clone;
-    type Balance: Zero + CheckedAdd + CheckedSub + Copy;
+pub struct Pallet {
+    pub balances: BTreeMap<String, u128>,
 }
 
-#[derive(Debug)]
-pub struct Pallet<T: Config> {
-    pub balances: BTreeMap<T::AccountId, T::Balance>,
-}
-
-impl<T: Config> Pallet<T> {
+impl Pallet {
     // Initialize a new Pallet with an empty balance map.
     pub fn new() -> Self {
         Self {
@@ -19,32 +12,32 @@ impl<T: Config> Pallet<T> {
         }
     }
     // Set the balance for a specific account.
-    pub fn set_balance(&mut self, who: &T::AccountId, amount: T::Balance) {
+    pub fn set_balance(&mut self, who: &String, amount: u128) {
         self.balances.insert(who.clone(), amount);
     }
     // Get the balance for a specific account, returning 0 if the account does not exist.
-    pub fn balance(&self, who: &T::AccountId) -> T::Balance {
-        *self.balances.get(who).unwrap_or(&T::Balance::zero())
+    pub fn balance(&self, who: &String) -> u128 {
+        *self.balances.get(who).unwrap_or(&0)
     }
     // Transfer a specified amount from a sender to a receiver.
     pub fn transfer(
         &mut self,
-        sender: T::AccountId,
-        receiver: T::AccountId,
-        amount: T::Balance,
+        sender: &String,
+        receiver: &String,
+        amount: u128,
     ) -> Result<(), &'static str> {
-        let sender_balance = self.balance(&sender);
-        let receiver_balance = self.balance(&receiver);
+        let sender_balance = self.balance(sender);
+        let receiver_balance = self.balance(receiver);
 
         let new_sender_balance = sender_balance
-            .checked_sub(&amount)
+            .checked_sub(amount)
             .ok_or("Not enough funds")?;
         let new_receiver_balance = receiver_balance
-            .checked_add(&amount)
+            .checked_add(amount)
             .ok_or("Overflow in receiver balance")?;
 
-        self.balances.insert(sender, new_sender_balance);
-        self.balances.insert(receiver, new_receiver_balance);
+        self.set_balance(&sender, new_sender_balance);
+        self.set_balance(&receiver, new_receiver_balance);
 
         Ok(())
     }
@@ -52,18 +45,9 @@ impl<T: Config> Pallet<T> {
 
 #[cfg(test)]
 mod tests {
-    use std::u128;
-
-    struct TestConfig;
-
-    impl super::Config for TestConfig {
-        type AccountId = String;
-        type Balance = u128;
-    }
-
     #[test]
     fn init_balance() {
-        let mut balance = super::Pallet::<TestConfig>::new();
+        let mut balance = super::Pallet::new();
         let alice = "Alice".to_string();
         let bob = "Bob".to_string();
 
@@ -75,19 +59,15 @@ mod tests {
 
     #[test]
     fn transfer_balance() {
-        let mut balance = super::Pallet::<TestConfig>::new();
+        let mut balance = super::Pallet::new();
+        let alice = "Alice".to_string();
+        let bob = "Bob".to_string();
 
-        assert_eq!(
-            balance.transfer("Alice".to_string(), "Bob".to_string(), 50),
-            Err("Not enough funds")
-        );
-        balance.set_balance(&"Alice".to_string(), 100);
-        assert_eq!(
-            balance.transfer("Alice".to_string(), "Bob".to_string(), 50),
-            Ok(())
-        );
-        assert_ne!(balance.balance(&"Alice".to_string()), 100);
-        assert_eq!(balance.balance(&"Alice".to_string()), 50);
-        assert_eq!(balance.balance(&"Bob".to_string()), 50);
+        assert_eq!(balance.transfer(&alice, &bob, 50), Err("Not enough funds"));
+        balance.set_balance(&alice, 100);
+        assert_eq!(balance.transfer(&alice, &bob, 50), Ok(()));
+        assert_ne!(balance.balance(&alice), 100);
+        assert_eq!(balance.balance(&alice), 50);
+        assert_eq!(balance.balance(&bob), 50);
     }
 }
